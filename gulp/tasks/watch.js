4cfe188a611322194b72055e160fdde87b0d87e5
Verdan777 <vereshhagin98@mail.ru>
2018-02-23 15:56:50 +0200

    workflow install

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,54 @@
+const gulp = require("gulp");
+const watch = require("gulp-watch");
+const browserSync = require("browser-sync").create();
+
+gulp.task("watch", () => {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        },
+        notify: {
+            styles: {
+                top: "auto",
+                bottom: "0"
+            }
+        }
+    });
+
+    watch("./src/**/*.pug", () => {
+        gulp.start("pugChanged");
+    });
+
+    watch("./src/scss/**/*.scss", () => {
+        gulp.start("cssInject");
+    });
+
+    watch("./src/js/**/*.js", () => {
+        gulp.start("jsChanged");
+    });
+
+    // watch("./wp/*.php", () => {
+    //     gulp.start("copyPHP");
+    // });
+
+    // watch("./dist/styles.css", () => {
+    //     gulp.start("copyCSS");
+    // });
+
+    // watch("./dist/*.js", () => {
+    //     gulp.start("copyJS");
+    // });
+});
+
+gulp.task("pugChanged", ["pugRender"], () => {
+    browserSync.reload();
+});
+
+gulp.task("cssInject", ["styles"], () => {
+    gulp.src("./dist/styles.css")
+        .pipe(browserSync.stream());
+});
+
+gulp.task("jsChanged", ["scripts"], () => {
+    browserSync.reload();
+});
